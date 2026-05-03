b9f17319ac96f6381d777d4a61da6c50ab5974ac
Pete Pongpeauk <kurtsiberg@gmail.com>
2026-05-03 04:58:08 -0400

    feat: improvements

--- src/components/report-user-button.tsx
+++ src/components/report-user-button.tsx
@@ -22,6 +22,7 @@ export function ReportUserButton({ user }: ReportUserButtonProps) {
       target={{
         contentType: "user",
         contentExternalId: user.externalId,
+
       }}
       trigger={
         <button
